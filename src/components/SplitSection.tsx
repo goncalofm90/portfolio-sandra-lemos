1fc6cd2f0188650e9640cac2770fcbb22782638c
goncalofm90 <goncalofm90@gmail.com@users.noreply.github.com>
2026-04-09 14:15:37 +0100

    fix text clipping

--- src/components/SplitSection.tsx
+++ src/components/SplitSection.tsx
@@ -14,7 +14,7 @@ export const SplitSection = ({
   return (
     <section
       id={sectionId}
-      className="lg:snap-start lg:snap-always w-full h-screen overflow-hidden border-t border-gray-100 px-4 py-6 sm:px-12 sm:py-8 lg:px-16 lg:snap-section"
+      className="lg:snap-start lg:snap-always w-full lg:h-screen lg:overflow-hidden border-t border-gray-100 px-4 py-6 sm:px-12 sm:py-8 lg:px-16 lg:snap-section"
     >
       <div className="flex h-full w-full flex-col gap-2 lg:flex-row lg:gap-2 items-center justify-center">
         {/* Image */}
